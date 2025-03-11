c6f5cbf58354c1dd7a0308e5e9d2fd495557c991
marciooseas <marciooseas@icloud.com>
2025-03-11 13:51:29 +0000

    Update and rename Biblioteca.php to index.php

---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+  <title>Painel </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+      <!-- CARDS EFEITOS-->
+      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+      <!-- CSS ligação -->
+      <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+
+ <?php
+ include "nav.php";
+ include "menu.php";
+ ?>
+ 
+<main id="main" class="main">
+<div class="pagetitle">
+     <h1>Painel</h1>
+     <nav>
+        <ol class="breadcrumb">
+           <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+           <li class="breadcrumb-item active">Painel</li>
+        </ol>
+     </nav>
+</div>
+        
+  <h1>Todos os Livros</h1>
+  <br>  
+  <h2>Construção Cívil</h2> 
+  <br>
+
+  <div class="slider owl-carousel">
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<br>
+<h2> Electrônica</h2> 
+<br>
+
+  <div class="slider owl-carousel">
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<h2>Informática </h2> 
+<br>
+
+  <div class="slider owl-carousel">
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<br>
+<h2>Mecânica</h2> 
+<br>
+<div class="slider owl-carousel">
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<br>
+<h2>Telecomunicações</h2> 
+<br>
+<div class="slider owl-carousel">
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="img"><img src="assets/img/site.jpg" alt=""></div>
+    <div class="content">
+      <div class="title">Titulo:</div>
+      <div class="sub-title">Autor:</div>
+      <p>Categoria:</p>
+      <div class="botao">
+        <button>Ler mais</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  $(".slider").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000, //2000ms = 2s;
+    autoplayHoverPause: true,
+  });
+</script>
+
+</main><!-- Fim main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  
